9b8cb87471da1dd50aea4656988fd3dfbf7bed74
hydroxion <whenrique26@gmail.com>
2020-01-04 17:09:26 -0300

    Status route created

---
+++ source/process.py
@@ -0,0 +1,23 @@
+from flask import Blueprint, request, jsonify
+
+
+from time import sleep
+
+
+from random import choice
+
+
+
+process = Blueprint('process', __name__)
+
+
+def status():
+    document = request.get_json(force=True, silent=False, cache=False)
+
+    if document.get('sleep') and type(document.get('sleep')) == int and document.get('sleep') > 0:
+        sleep(document.get('sleep'))
+
+    return jsonify({'status': choice([True, False])}), 200
+
+
+process.add_url_rule('/status', view_func=status, methods=['POST'])
