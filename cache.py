08fa14e617758f6f0fdd40e53b6a11f986011378
hydroxion <whenrique26@gmail.com>
2020-01-04 12:23:38 -0300

    Place source code to a specific folder

--- cache.py
+++
@@ -1,6 +0,0 @@
-from redis import Redis
-
-
-# Change the host to '0.0.0.0' if you start the application
-# locally, e.g, on virtual environment or through Docker file
-client = Redis(host='redis', port=6379, db=0)
